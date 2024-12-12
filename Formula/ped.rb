1576f602c1abcd4c2be665ca35b5edf93bfaa2bf
David Edwards <david.l.edwards@gmail.com>
2024-12-11 19:47:21 -0600

    prepare for 0.4.0 release

--- Formula/ped.rb
+++ Formula/ped.rb
@@ -1,15 +1,15 @@
 class Ped < Formula
   desc "The pedestrian text editor"
   homepage "https://github.com/davidledwards/ped"
-  version "0.3.1"
+  version "0.4.0"
 
   if OS.linux? || OS.mac?
     if Hardware::CPU.intel?
       url "https://github.com/davidledwards/ped/releases/download/v" + version + "/ped-" + version + "-x86_64-unix.tar.gz"
-      sha256 "cbb46686b973474fe01271fea96e9e079f5a05a21125b5d3593a4ce8c741901a"
+      sha256 "a38b1171fddeb9f525b5b11dd9c2916cc7b6bfdfd0b47e5905d315d02854d17a"
     elsif Hardware::CPU::arm?
       url "https://github.com/davidledwards/ped/releases/download/v" + version + "/ped-" + version + "-aarch64-unix.tar.gz"
-      sha256 "a962d07c0f7420e97b8e5c725b4936ddecd6e7a74d5fe47e8352d4c9585b34ca"
+      sha256 "868efa8ab6bfb8a44c524b89cd1fb03238633ba5c8d1eb9cdf66613e5a10b7a1"
     end
   end
 
