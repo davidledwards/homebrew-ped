d8b1f2a0dc3b025c8eb7df0e240ffcbe3d812900
David Edwards <david.l.edwards@gmail.com>
2025-08-16 09:38:59 -0500

    annotate formula for automated updates when new releases are published

--- Formula/ped.rb
+++ Formula/ped.rb
@@ -5,14 +5,13 @@ class Ped < Formula
   url_prefix = "https://github.com/davidledwards/ped/releases/download/v#{version}/ped-#{version}"
 
   on_macos do
-    on_intel do
-      url "#{url_prefix}-x86_64-apple-darwin.tar.gz"
-      sha256 "9d62c582b3f01be3d9b5b58d17c4cb5e9e2e37e9a31d43afeb62999c22bc9374"
-    end
-
     on_arm do
       url "#{url_prefix}-aarch64-apple-darwin.tar.gz"
-      sha256 "f91434c7595a71667725b8c9b48646c589051fc03a31b9dd9d664fd585b9462d"
+      sha256 "f91434c7595a71667725b8c9b48646c589051fc03a31b9dd9d664fd585b9462d" # aarch64-apple-darwin
+    end
+    on_intel do
+      url "#{url_prefix}-x86_64-apple-darwin.tar.gz"
+      sha256 "9d62c582b3f01be3d9b5b58d17c4cb5e9e2e37e9a31d43afeb62999c22bc9374" # x86_64-apple-darwin
     end
   end
 
@@ -20,10 +19,10 @@ class Ped < Formula
     on_intel do
       if Hardware::CPU.is_64_bit?
         url "#{url_prefix}-x86_64-unknown-linux-gnu.tar.gz"
-        sha256 "e52162aabf103007823667a5430355ab854fdd104efb9ebc8f8a353f14c12d13"
+        sha256 "e52162aabf103007823667a5430355ab854fdd104efb9ebc8f8a353f14c12d13" # x86_64-unknown-linux-gnu
       else
         url "#{url_prefix}-i686-unknown-linux-gnu.tar.gz"
-        sha256 "fbf361da1cbf93d9208b336b99ca634b4e16c123107c8c331751b8068295881f"
+        sha256 "fbf361da1cbf93d9208b336b99ca634b4e16c123107c8c331751b8068295881f" # i686-unknown-linux-gnu
       end
     end
   end
